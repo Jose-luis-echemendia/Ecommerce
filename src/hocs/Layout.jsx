090e351e3d3af59367be3050394b69608cb4d982
Jose Luis Echemendia López <josee@estudiantes.uci.cu>
2024-12-28 15:41:46 -0500

    Update Layout.jsx

--- src/hocs/Layout.jsx
+++ src/hocs/Layout.jsx
@@ -2,9 +2,9 @@ import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
 import { Navbar } from "../components/navigation/Navbar"
 import { Footer } from "../components/footer/footer"
+import PropTypes from 'prop-types'
 
 export const Layout = ({ children }) => {
-
 
   return (
     <div className="flex flex-col">
@@ -15,3 +15,8 @@ export const Layout = ({ children }) => {
     </div>
   )
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
